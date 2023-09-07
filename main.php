48261a947478a5aaa96f8338d95c9f89a76ffd44
hermanbenedict <35999514+hermanbenedict@users.noreply.github.com>
2023-09-07 11:26:18 +0300

    Update main.php

--- main.php
+++ main.php
@@ -1,22 +1,12 @@
 <?php
-// Include the TwitterAPI class
-require_once('TwitterAPI.php');
 
 // Set up your Twitter API credentials
-$consumerKey = 'Your consumer key';
-$consumerSecret = 'Your consumer secret key';
-$accessToken = 'Your Access token';
-$accessTokenSecret = 'Your access token secret';
+$consumerKey = 'consumerKey';
+$consumerSecret = 'consumerSecret';
+$accessToken = 'accessToken';
+$accessTokenSecret = 'accessTokenSecret';
 
-// Your Twitter post
-$text = "Hello world this is just a testing post";
+$text = 'This is testing post';
 
-// Create an instance of the TwitterAPI class
-$twitterAPI = new TwitterAPI($consumerKey, $consumerSecret, $accessToken, $accessTokenSecret);
-
-// Post the tweet using the API
-$response = $twitterAPI->postTweet($text);
-
-// Display the API response
-echo $response;
+require "TwitterAPI.php";
 ?>
