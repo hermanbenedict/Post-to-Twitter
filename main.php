470cdc09172d8647a67f1930c617e7648ddc9b28
hermanbenedict <35999514+hermanbenedict@users.noreply.github.com>
2024-05-20 13:46:06 +0300

    Update main.php

--- main.php
+++ main.php
@@ -6,7 +6,15 @@ $consumerSecret = 'consumerSecret';
 $accessToken = 'accessToken';
 $accessTokenSecret = 'accessTokenSecret';
 
-$text = 'This is testing post';
+// Define your tweet text and optional image path
+$text = 'Your tweet text here';
 
-require "TwitterAPI.php";
+$imagePath = 'image.jpg'; // Update this with the path to your image
+
+if($imagePath ==""){
+require "postText.php";
+}
+elseif($imagePath !=""&&$text !=""){
+    require "postImage.php";
+}
 ?>
